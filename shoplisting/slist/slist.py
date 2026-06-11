561ca884a9ff6f57343e71a2ee988bdae83634ea
pseu <pseudobyte@gmail.com>
2026-06-11 16:14:42 -0600

    implemented alphabetical category sorting for slist gen added config integration for min/max slist heading

--- shoplisting/slist/slist.py
+++ shoplisting/slist/slist.py
@@ -1,6 +1,7 @@
 from shoplisting.model import Recipe, Category, Ingredient, CategoryDisplay
 from datetime import date, datetime, timedelta
 import snakemd
+from shoplisting.config.config import ConfigTree, load_config
 
 class MDCategory():
     def __init__(self, category, parent):
@@ -121,8 +122,9 @@ class MDList():
                 data['categories'].append(subcat)
         return data
 
-def render_category(category, doc, level=0, dateless=False):
-    doc.add_heading(category['name'], 2 if level == 0 else min(4,2+level))
+def render_category(category, doc, cfg, level=0, dateless=False):
+    max_head, min_head = cfg['slist.max_heading'], cfg['slist.min_heading']
+    doc.add_heading(category['name'], max_head if level == 0 else min(min_head,max_head+level))
     ing_list = []
     for ing in category['ingredients']:
         name = ing['name']
@@ -139,10 +141,11 @@ def render_category(category, doc, level=0, dateless=False):
     doc.add_block(
         snakemd.MDList(ing_list, ordered=False)
     )
+    category['categories'].sort(key=lambda x:x['name'])
     for subcat in category['categories']:
-        render_category(subcat, doc, level+1, dateless)
+        render_category(subcat, doc, cfg, level+1, dateless)
 
-def render_markdown(slist):
+def render_markdown(slist, cfg):
     doc = snakemd.Document()
     start_date = slist['start_date']
     dateless = False
@@ -151,7 +154,7 @@ def render_markdown(slist):
     else:
         start_date = ''
         dateless = True
-    doc.add_heading(f'Shopping List {start_date}', 2)
+    doc.add_heading(f'Shopping List {start_date}', cfg['slist.max_heading'])
     rows = []
     notes = []
     for recipe in slist['recipes']:
@@ -165,12 +168,20 @@ def render_markdown(slist):
            doc.add_block(
                snakemd.MDList([snakemd.Inline(recipe['note'], italics=True)], ordered=False)
            )
+    slist['categories'].sort(key=lambda x:x['name'])
     for cat in slist['categories']:
         doc.add_horizontal_rule()
-        render_category(cat, doc, level=0, dateless=dateless)
+        render_category(cat, doc, cfg, level=0, dateless=dateless)
     return str(doc)
 
+default_slist_config = {
+    'min_heading': 4,
+    'max_heading': 2
+}
+
 def generate_slist(recipes, single_items, start_date):
+    cfg = load_config()
+    cfg.default = ConfigTree({'slist': default_slist_config })
     slist = MDList(start_date)
     for recipe_id in recipes:
         slist.add_recipe(recipe_id)
@@ -178,5 +189,5 @@ def generate_slist(recipes, single_items, start_date):
         slist.add_single_item(ing_id)
     slist.construct_categories()
     slist = slist.emit_list()
-    md = render_markdown(slist)
+    md = render_markdown(slist, cfg)
     return md
