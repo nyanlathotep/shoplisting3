e8fc57332ff3c9ebbe9696e169748f1c8819a214
pseu <pseudobyte@gmail.com>
2026-06-18 19:14:21 -0600

    moved flash error and modal handling into common lib

--- shoplisting/static/common.js
+++ shoplisting/static/common.js
@@ -1,8 +1,10 @@
-function confirmModal({ 
-    modalId, 
-    title = "Confirm", 
-    body = "Are you sure?", 
-    onConfirm = () => {} 
+function confirm_modal({ 
+    modalId,
+    title = "Confirm",
+    body = "Are you sure?",
+    confirmText = "Delete",
+    confirmCls = "btn btn-danger",
+    onConfirm = () => {}
 }) {
     const $modal = $(modalId);
     const $title = $modal.find('.modal-title');
@@ -12,16 +14,46 @@ function confirmModal({
     // Set content
     $title.text(title);
     $body.text(body);
+    $confirmBtn.text(confirmText);
+    $confirmBtn.attr('class', 'confirm-btn');
+    $confirmBtn.addClass(confirmCls);
 
     // Remove any previous click handlers
-    $confirmBtn.off('click');
+    $confirmBtn.off('click.slmodal');
 
     // Add the new one
-    $confirmBtn.on('click', function() {
+    $confirmBtn.on('click.slmodal', function() {
         $modal.modal('hide');
         onConfirm();
     });
 
     // Show modal
     $modal.modal('show');
+}
+
+function flashError(message) {
+    const container = $('.container, .card-body').first();
+
+    // Remove older alerts if more than 3 exist
+    const alerts = container.find('.alert-danger');
+    if (alerts.length >= 3) {
+        // remove the oldest (last one)
+        alerts.last().remove();
+    }
+
+    const alert = $(`
+        <div class="alert alert-danger alert-dismissible fade show mt-3" role="alert">
+            ${message}
+            <button type="button" class="close" data-dismiss="alert">
+                <span>&times;</span>
+            </button>
+        </div>
+    `);
+
+    container.prepend(alert);
+
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 }
